deefe654690c4d38ec0ad0c2965fd0fe3690957a
Gabriel Costa <gabriel.costa@concrete.com.br>
2017-08-09 15:04:43 -0300

    Remove default parameter values from Delta.findDelta

--- core/src/main/java/gabrielcosta/core/Delta.kt
+++ core/src/main/java/gabrielcosta/core/Delta.kt
@@ -2,7 +2,7 @@ package gabrielcosta.core
 
 class Delta {
 
-    fun findDelta(a: Double = 0.0, b: Double = 0.0, c: Double = 0.0) : Double {
+    fun findDelta(a: Double, b: Double, c: Double) : Double {
         TODO("FIND THE DELTA")
     }
 
